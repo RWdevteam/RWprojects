5c23b21af966e98a52a224bbf6e1cd0fe2d91631
RWdevteam <42911968+RWdevteam@users.noreply.github.com>
2018-09-02 22:11:29 +0800

    RW authenticator
    RW authenticator

---
+++ RW_auth.php
@@ -0,0 +1,14 @@
+
+<?php
+echo $_POST['pass'];
+if($_POST['pass']=="pIZZ4withFINE4ppl3sandBURG3RS"){
+	echo "Correct Password <br>";
+}else{
+	
+	echo "Wrong Password <br>";
+}
+
+?>
+
+
+RWauthenticator 2018 
